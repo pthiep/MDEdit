ef6660987e8609f8786f4836265c0f963e3f217a
phantanhiep <phantanhiepnt@gmail.com>
2018-10-15 00:51:41 +0700

    tạo richtextbox

--- MDEdit/MainWindow.xaml.cs
+++ MDEdit/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,17 +13,50 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MahApps.Metro.Controls;
 
 namespace MDEdit
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow, INotifyPropertyChanged
     {
+        private string xMDSource;
+        public string XMDSource {
+            get => xMDSource;
+            set {
+                xMDSource = value;
+                OnPropertyChanged("XMDSource");
+            }            
+        }          
+
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = this;
+            XMDSource = "This is MD";
         }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string newName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(newName));
+            }
+        }
+
+        private void TestString_Click(object sender, RoutedEventArgs e)
+        {
+            //string richText = new TextRange(Richtextbox.Document.ContentStart, Richtextbox.Document.ContentEnd).Text;
+            //MessageBox.Show(richText);
+            MessageBox.Show(xMDSource);
+        }
+
+        //private void rtbXMDSource_TextChanged(object sender, TextChangedEventArgs e)
+        //{
+        //    XMDSource = rtbXMDSource.Text;
+        //}
     }
 }
